75530a2aa74161e38872610f6d280c5111fd76ec
neils94 <63812857+neils94@users.noreply.github.com>
2022-02-20 11:13:21 -0800

    Add files via upload

---
+++ definitions.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std: //make standard library accessible to use names
+
+class Agent {
+
+Agent::Agent() //agent class constructor
+
+//parameters to change in Agent class include action size(int)
+//in order to get an output size for neural network 
+//no need to include state size because state size is continuous
+//and neural networks will acept the output of CNN instead of
+//traditional state size with linear networks input
+
+
+public:
+int Agent::action_size() //public access to change action_size
+    
+
+}
+
+class Environment{
+
+    Environment::Environment() //class constructor for environment
+
+
+    public:
+        float trade_result() const;
+
+    private:
+        float balance;
+        
+
+
+
+
+}
---
+++ model.cpp
@@ -0,0 +1,8 @@
+//transformers will be used to identify/recognize objects in images
+//CNN and transformer can be paired to take feature maps of CNN
+
+//Patches obtained from feature maps can have spatial size 1x1
+//which means that the input sequence is obtained by simply 
+//flattening the spacial dimensions of the feature map and
+//projecting to the Transformer dimension
+
